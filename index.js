eb9b7fd0f658107339c74323aaba803b50f684b2
MaksymZaluskiy <zmax1301@gmail.com>
2025-08-08 17:44:05 +0200

    express_static

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const path = require("path")
 const app = express()
 const port = 3000
 
+
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.use(express.static('/styles'))
+app.get("/popularProduct", (req, res) => {
+    res.sendFile(path.join(__dirname, "pages", "popularProduct.html"));
+});
+
+app.use(express.static(__dirname + "/public"));
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
