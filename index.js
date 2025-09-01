e9859c6d1c9944f848af8e350ef701886af24cf1
MaksymZaluskiy <zmax1301@gmail.com>
2025-09-01 13:38:30 +0200

    new_users_functioanal

--- index.js
+++ index.js
@@ -1,66 +1,79 @@
-const express = require('express')
-const path = require("path")
-const app = express()
-const fs = require("fs")
-require("dotenv").config()
-const nodemailer = require("nodemailer")
-const port = process.env.port
+const express = require("express");
+const path = require("path");
+const app = express();
+const fs = require("fs");
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+const port = process.env.port;
 
 app.use(express.static(__dirname + "/public"));
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-
 app.post("/", (req, res) => {
-    const { email } = req.body
-    const date =  new Date().toISOString().split('T')[0]
-    const data = `${email}, ${date} \n`
-        fs.appendFile('leads.txt', data, (err) => {
+    const { email } = req.body;
+    const date = new Date().toISOString().split("T")[0];
+    const data = `${email}, ${date} \n`;
+    fs.appendFile("leads.txt", data, (err) => {
         if (err) throw err;
-        res.send('Користувача зареєстровано та дані збережено.');
+        res.send("Користувача зареєстровано та дані збережено.");
     });
+});
 
-})
+app.post("/about", (req, res) => {
+    const { email } = req.body;
+    const date = new Date().toISOString().split("T")[0];
+    const data = `${email}, ${date} \n`;
+    fs.appendFile("leads.txt", data, (err) => {
+        if (err) throw err;
+        res.send("Користувача зареєстровано та дані збережено.");
+    });
+});
 
+app.post("/bestProduct", (req, res) => {
+    const { email } = req.body;
+    const date = new Date().toISOString().split("T")[0];
+    const data = `${email}, ${date} \n`;
+    fs.appendFile("leads.txt", data, (err) => {
+        if (err) throw err;
+        res.send("Користувача зареєстровано та дані збережено.");
+    });
+});
 
-
-    let transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.GMAIL_EMAIL,
-            pass: process.env.GMAIL_PASS
-        }
-})
+let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.GMAIL_EMAIL,
+        pass: process.env.GMAIL_PASS,
+    },
+});
 
 app.post("/administration", (req, res) => {
-    const { email } = req.body
-    res.send(email)
+    const { email } = req.body;
+    res.send(email);
 
-    fs.readFile('leads.txt', 'utf8', (err, data) => {
-        data
-            .split("\n")
-            .filter(line => line != "")
+    fs.readFile("leads.txt", "utf8", (err, data) => {
+        data.split("\n")
+            .filter((line) => line != "")
             .map((line, index) => {
-
                 let mailOptions = {
                     from: process.env.GMAIL_EMAIL,
                     to: line,
                     subject: "Тестовий лист",
                     text: `fefefefefefefe`,
+                };
 
-}
-
-    transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-            return console.log(err)
-        }
-        console.log("Лист успішно відправлено", info.response)
-    })
-        })
-    })
-})
+                transporter.sendMail(mailOptions, (err, info) => {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    console.log("Лист успішно відправлено", info.response);
+                });
+            });
+    });
+});
 
 //=========
 
@@ -99,14 +112,18 @@ app.get("/administration", (req, res) => {
 //----------------------
 
 app.get("/lead", (req, res) => {
-    fs.readFile('leads.txt', 'utf8', (err, data) => {
+    fs.readFile("leads.txt", "utf8", (err, data) => {
         const array = data
             .split("\n")
-            .filter(line => line != "")
+            .filter((line) => line != "")
             .map((line, index) => {
-                return `<li><span class="index">${index + 1}</span> <span class="email">${line.split(",")[0]}</span><span class="date">${line.split(",")[1]}</span></li>`
+                return `<li><span class="index">${
+                    index + 1
+                }</span> <span class="email">${
+                    line.split(",")[0]
+                }</span><span class="date">${line.split(",")[1]}</span></li>`;
             })
-            .join("")
+            .join("");
 
         res.send(`
     <!DOCTYPE html>
@@ -124,12 +141,10 @@ app.get("/lead", (req, res) => {
             ${array}
         </body>
     </html>
-            `)
-
+            `);
+    });
 });
-})
-
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
